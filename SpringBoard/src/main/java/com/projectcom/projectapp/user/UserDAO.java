0f8165e6b0745ef572a6e3607e0c98e258a48b0f
patcha98 <patcha1@naver.com>
2021-12-10 01:42:45 +0900

    lol

---
+++ SpringBoard/src/main/java/com/projectcom/projectapp/user/UserDAO.java
@@ -0,0 +1,22 @@
+package com.projectcom.projectapp.user;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.projectcom.projectapp.board.UserVO;
+
+@Repository
+public class UserDAO {
+	@Autowired
+	SqlSessionTemplate sqlSession;
+	public UserVO getUser(UserVO vo) {
+		return sqlSession.selectOne("User.getUser", vo);
+	} 
+	
+	public int insertAdmin() {
+		int result = sqlSession.insert("User.insertAdmin");
+		return result;
+	}
+
+}
---
+++ SpringBoard/src/main/java/com/projectcom/projectapp/user/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.projectcom.projectapp.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projectcom.projectapp.board.UserVO;
+
+@Service
+public class UserServiceImpl {
+	@Autowired
+	UserDAO userDAO;
+	public UserVO getUser(UserVO vo) {
+		return userDAO.getUser(vo);
+	}
+	
+	public int insertAdmin() {
+		return userDAO.insertAdmin();
+	}
+}
